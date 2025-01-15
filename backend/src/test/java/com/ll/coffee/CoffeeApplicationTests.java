176eafe834f7ad2fedcdccf15ffd92f3176bd157
jsh4635 <49675179+jsh4635@users.noreply.github.com>
2025-01-15 14:17:30 +0900

    work

--- backend/src/test/java/com/ll/coffee/CoffeeApplicationTests.java
+++ backend/src/test/java/com/ll/coffee/CoffeeApplicationTests.java
@@ -1,6 +1,5 @@
 package com.ll.coffee;
 
-import com.ll.coffee.member.Member;
 import com.ll.coffee.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +13,6 @@ class CoffeeApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Member member = userService.save("admin", "admin@admin.com", "admin");
-
 
 	}
 
