32712868b11d1563d1d19db9b9ceb645aa275fbd
jsh4635 <49675179+jsh4635@users.noreply.github.com>
2025-01-16 19:14:11 +0900

    cors add

--- backend/src/main/java/com/ll/coffee/controller/HomeController.java
+++ backend/src/main/java/com/ll/coffee/controller/HomeController.java
@@ -1,18 +1,11 @@
 package com.ll.coffee.controller;
 
-import com.ll.coffee.OrderMenu.OrderMenuDto;
 import com.ll.coffee.repository.OrderRepository;
 import com.ll.coffee.service.OrderMenuService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author shjung
@@ -30,7 +23,7 @@ public class HomeController {
         return "login_form";
     }
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    /*@PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/list")
     public String list(Model model,
                        @RequestParam(value = "page", defaultValue = "0")int page,
@@ -41,13 +34,13 @@ public class HomeController {
         model.addAttribute("paging", paging);
         model.addAttribute("kw", kw);
         return "home";
-    }
+    }*/
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    /*@PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/detail/{email}")
     public String detail(Model model,
                       ){
 
         return "order_detail";
-    }
+    }*/
 }
