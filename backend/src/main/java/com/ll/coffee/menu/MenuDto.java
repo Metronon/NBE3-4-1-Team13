d4a708997d08cde7412470e71ba2e7fd07eebbe5
yuunha <h000414@naver.com>
2025-01-16 09:58:48 +0900

    MenuDto 추가

---
+++ backend/src/main/java/com/ll/coffee/menu/MenuDto.java
@@ -0,0 +1,15 @@
+package com.ll.coffee.menu;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class MenuDto {
+    private Long id;
+    private String name;
+    private String type;
+    private Integer price;
+}
