6419e8c5d98e6b73dff0736b016b2bc72b84f397
jsh4635 <49675179+jsh4635@users.noreply.github.com>
2025-01-17 09:07:48 +0900

    work

--- backend/src/main/java/com/ll/coffee/controller/HomeController.java
+++ backend/src/main/java/com/ll/coffee/controller/HomeController.java
@@ -15,7 +15,6 @@ package com.ll.coffee.controller;
 //import org.springframework.web.bind.annotation.RequestParam;
 //
 
-import com.ll.coffee.OrderMenu.OrderMenuDto;
 import com.ll.coffee.OrderMenu.OrderMenuWithOrderDto;
 import com.ll.coffee.repository.OrderRepository;
 import com.ll.coffee.service.OrderMenuService;
@@ -28,7 +27,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 ///**
