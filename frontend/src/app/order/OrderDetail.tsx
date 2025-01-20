700878a2a7abf13a323f8b14cc9d7e9b65bc6279
Metronon <bumpercar07@gmail.com>
2025-01-20 11:02:22 +0900

    modify css & OrderDetail

--- frontend/src/app/order/OrderDetail.tsx
+++ frontend/src/app/order/OrderDetail.tsx
@@ -5,20 +5,24 @@ import React from "react";
 const OrderDetail = ({ filteredProducts }) => {
     return (
         <div className="order-detail">
-            {filteredProducts.map((product) => (
-                <div key={product.menuId} className="order-item">
-                    <img
-                        src={product.image}
-                        alt={product.menuName}
-                        className="product-img"
-                    />
-                    <p className="order-item-name">{product.menuName}</p>
-                    <span className="order-quantity">{product.menuCount}</span>
-                    <span className="order-price">
-                        {(product.menuPrice * product.menuCount).toLocaleString("ko-kr")} 원
-                    </span>
-                </div>
-            ))}
+            {filteredProducts.length === 0 ? (
+                <p className="order-detail-notice">장바구니가 비어있습니다</p>
+            ) : (
+                filteredProducts.map((product) => (
+                    <div key={product.menuId} className="order-item">
+                        <img
+                            src={product.image}
+                            alt={product.menuName}
+                            className="product-img"
+                        />
+                        <p className="order-item-name">{product.menuName}</p>
+                        <span className="order-quantity">{product.menuCount}</span>
+                        <span className="order-price">
+                            {(product.menuPrice * product.menuCount).toLocaleString("ko-kr")} 원
+                        </span>
+                    </div>
+                ))
+            )}
         </div>
     );
 };
